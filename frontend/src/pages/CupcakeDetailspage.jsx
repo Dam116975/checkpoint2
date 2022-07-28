0c979fadc55fc58438bcaa234eb0fe8a81816dbd
Dam116975 <76938706+Dam116975@users.noreply.github.com>
2022-07-28 10:20:47 +0200

    test7

--- frontend/src/pages/CupcakeDetailspage.jsx
+++ frontend/src/pages/CupcakeDetailspage.jsx
@@ -1,28 +1,56 @@
-// import { useParams } from "react-router-dom";
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// function CupcakeDetailspage() {
-//   const { id } = useParams();
+function CupcakeDetailspage() {
+  const { id } = useParams();
 
-//   const [cupcake, setCupcake] = useState([]);
+  const [cupcake, setCupcake] = useState([]);
 
-//   useEffect(() => {
-//     axios
-//       .get(`http://localhost:4000/cupcakes/${id}`)
-//       .then((datas) => setCupcake(datas.data));
-//   }, []);
-//   return (
-// <>
-//       {
-//          cupcake &&
-//         <div>
-//           <img src={cupcake.url} alt="accessory" />
-//           <h1>{cupcake.name}</h1>
-//         </div>
-//       }
-//       </>
-//   );
-// }
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/cupcakes/${id}`)
+      .then((datas) => setCupcake(datas.data));
+  }, []);
+  return (
+    <div>
+      {cupcake && (
+        <div key={cupcake.id} className="cupcake-container">
+          <div className="cupcake">
+            <img src={cupcake.url} alt={cupcake.name} />
+            <div className="cream">
+              <div
+                className="cream-1"
+                style={{ backgroundColor: cupcake.color1 }}
+              />
+              <div
+                className="cream-2"
+                style={{ backgroundColor: cupcake.color2 }}
+              />
+              <div
+                className="cream-3"
+                style={{
+                  backgroundColor: cupcake.color3,
+                }}
+              />
+            </div>
+            <div className="bottom">
+              <div className="bottom-in">
+                <div className="face">
+                  <div className="eyes">
+                    <div className="left-eye" />
+                    <div className="right-eye" />
+                  </div>
+                  <div className="mouth" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="cupcake-name">{cupcake.name}</div>
+        </div>
+      )}
+    </div>
+  );
+}
 
-// export default CupcakeDetailspage;
+export default CupcakeDetailspage;
